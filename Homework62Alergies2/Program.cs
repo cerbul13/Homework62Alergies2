6b7f5e94eb2d427e2970c7c18ce6135fb1f8d89a
cerbul13 <cerbadan@yahoo.com>
2022-01-10 12:13:27 +0200

    Add project files.

---
+++ Homework62Alergies2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Homework62Alergies2
+{
+    public enum Alergies
+    {
+        eggs=0,
+        peanuts=1,
+        shellfish=2,
+        strawberries=3,
+        tomatoes=4,
+        chocolate=5,
+        pollen=6,
+        cats=7
+    }
+    class Program
+    {
+        public static bool IsBitSet(int b, int pos)
+        {
+            return (b & (1 << pos)) != 0;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti score-ul lui Tom: ");
+            int score = int.Parse(Console.ReadLine());
+            for (int i=0;i<8;i++)
+            {
+                if (IsBitSet(score,i))
+                {
+                    Console.WriteLine($"Tom este alergic la {Enum.GetName(typeof(Alergies),i)}.");
+                }
+            }
+        }
+    }
+}
